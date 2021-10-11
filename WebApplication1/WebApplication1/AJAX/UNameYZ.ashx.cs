ff2f183ec4cdc3f22b9785ee43dc0f9f84f41f21
Paige <2624400269@qq.com>
2021-10-11 16:06:58 +0800

    10.11(1) AJAX和Form分别完成”输入的用户名是否存在的验证“功能

---
+++ WebApplication1/WebApplication1/AJAX/UNameYZ.ashx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Models;
+
+namespace WebApplication1.AJAX
+{
+    /// <summary>
+    /// UNameYZ 的摘要说明
+    /// </summary>
+    public class UNameYZ : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            /* -- 验证用户名是否存在，实现 AJAX -- */
+            string srnr = context.Request.QueryString["UName"];
+            if (srnr != "")
+            {
+                string sql = string.Format("SELECT * FROM Users WHERE UName='{0}'", srnr);
+                using (FXEntities fx = new FXEntities())
+                {
+                    if (fx.Database.SqlQuery<Users>(sql).ToList().Count > 0)
+                    {
+                        context.Response.Write("您输入的用户名:" + srnr + "已存在！请重新输入！");
+                    }
+                    else
+                    {
+                        context.Response.Write("您的用户名:" + srnr + "可以进行注册！");
+                    }
+                }
+            }
+            else
+            {
+                context.Response.Write("请输入用户名！");
+            }
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
--- WebApplication1/WebApplication1/Controllers/DefaultController.cs
+++ WebApplication1/WebApplication1/Controllers/DefaultController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Models;
 
 namespace WebApplication1.Controllers
 {
@@ -11,7 +12,60 @@ namespace WebApplication1.Controllers
         // GET: Default
         public ActionResult Index()
         {
+            /* -- 获取Users表数据 -- */
+            using (FXEntities fx = new FXEntities())
+            {
+                ViewBag.UName = fx.Users.ToList();
+            }
             return View();
         }
+
+        /* -- 验证用户名是否存在 -- */
+        [HttpPost] /* -- 非三层非AJXA版 -- */
+        public ActionResult Index(string UName)
+        {
+            using (FXEntities fx = new FXEntities())
+            {
+                /* -- 获取Users表数据 -- */
+                ViewBag.UName = fx.Users.ToList();
+
+                if (UName != "")
+                {
+                    string sql = string.Format("SELECT * FROM Users WHERE UName='{0}'", UName);
+                    if (fx.Database.SqlQuery<Users>(sql).ToList().Count > 0)
+                    {
+                        ViewBag.TC = "zhi";
+                        ViewBag.name = UName;
+                    }
+                    else
+                    {
+                        ViewBag.TC = "kong2";
+                        ViewBag.name = UName;
+                    }
+                }
+                else
+                {
+                    ViewBag.TC = "kong";
+                }
+
+            }
+            return View();
+        }
+
+
+        /* -- 验证用户名是否存在(非三层AJAX版) -- */
+        public ActionResult Index2()
+        {
+            using (FXEntities fx = new FXEntities())
+            {
+                /* -- 获取Users表数据 -- */
+                ViewBag.UName = fx.Users.ToList();
+            }
+
+            return View();
+        }
+
+
+
     }
 }
